c29ca4552108b6d6a96dcaa0b71a0d6e000e110b
zqy <luffy.zhouqingyi@icloud.com>
2023-01-20 16:32:45 -0800

    version 1.0

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './css/reset.css';
+import './index.css';
+import { Provider } from 'react-redux';
+import store from './Store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
+  </Provider>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
